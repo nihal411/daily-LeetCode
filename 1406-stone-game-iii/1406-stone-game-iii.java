7f39de04dc01bd3f03bc0e1202d2775d8034a71b
Nihal <mdnihal7860srnf@gmail.com>
2026-08-03 19:07:25 +0530

    Time: 10 ms (83.24%), Space: 85.3 MB (76.7%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -0,0 +1,18 @@
+class Solution{
+    public String stoneGameIII(int[] stoneValue){
+        int n=stoneValue.length;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,Integer.MIN_VALUE);
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            int sum=0;
+            for(int j=i;j<Math.min(n,i+3);j++){
+                sum+=stoneValue[j];
+                dp[i]=Math.max(dp[i],sum-dp[j+1]);
+            }
+        }
+        if(dp[0]>0)return "Alice";
+        if(dp[0]<0)return "Bob";
+        return "Tie";
+    }
+}
